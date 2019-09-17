364fb1cea61a937891f9ce7fbf042675ede9b9bd
Simon Walker <s.r.walker101@googlemail.com>
2019-09-17 22:12:11 +0100

    Start gui package

---
+++ gui/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nntrainer_gui",
+    author="Simon Walker",
+    author_email="s.r.walker101@googlemail.com",
+    version="0.0.1",
+    packages=find_packages(),
+)
