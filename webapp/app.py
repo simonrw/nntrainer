12fbea2a498b871bd090e27c60b7b49c44600099
Simon Walker <s.r.walker101@googlemail.com>
2019-09-18 22:45:37 +0100

    Implement architectures endpoint

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask("nntrainer")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/api/architectures")
+def api_architectures():
+    return jsonify(["ResNet50", "VGG16"])
