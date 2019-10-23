99ce3b6702ccd535bff5ae2f9aaf20289cbb15ae
Simon Walker <s.r.walker101@googlemail.com>
2019-10-23 23:47:52 +0100

    Add install requires to setup.py

--- gui/setup.py
+++ gui/setup.py
@@ -7,4 +7,10 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     entry_points={"gui_scripts": ["nntrainer = nntrainer.app:main"]},
+    install_requires=[
+        "tensorflow>=2",
+        "numpy",
+        "pyqt5",
+        "pyqtgraph",
+        ],
 )
